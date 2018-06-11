04733fe88e2af25dc4dedf6c1c3a1b67524b13ce
Trevor Seitz <trevinla@gmail.com>
2018-06-11 14:09:51 -0400

    4 migrations completed

--- db/migrate/20180611171547_create_orders.rb
+++ db/migrate/20180611171547_create_orders.rb
@@ -1,4 +1,12 @@
 class CreateOrders < ActiveRecord::Migration
   def change
+    create_table :orders do |t|
+      t.integer :store_id
+      t.integer :user_id
+      t.integer :burrito_id
+      t.integer :quantity
+      t.decimal :item_total
+      t.decimal :order_total
+    end
   end
 end
--- db/migrate/20180611171624_create_users.rb
+++ db/migrate/20180611171624_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.integer :store_id
+      t.integer :order_ids
+    end
   end
 end
