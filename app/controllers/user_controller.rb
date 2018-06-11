30182ef32108a6ad821e0e73d0100a33abd676c7
Trevor Seitz <trevinla@gmail.com>
2018-06-11 16:07:16 -0400

    building user_controler - signup, login, logout done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,33 @@
 class UserController < ApplicationController
 
-    get '/users' do
-      @users = User.all
+  get "/users" do
+    @users = User.all
 
-      erb :'users/index'
-    end
+    erb :"users/index"
+  end
+
+  get "/users/new" do
+    # sign up a new user
+    if is_logged_in?
+      # if the user is already logged in - don't allow them to see the "new user" page
+      # instead, redirect to their store's index page
+      redirect "/store/:id/index"
+    else
+      erb :"users/new"
+  end
+
+  get "/login" do
+    # user login page
+    if is_logged_in?
+      # if the user is already logged in - don't allow them to see the "login" page
+      # instead, redirect to their store's index page
+      redirect "/store/:id/index"
+    else
+      erb :"/users/login"
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/users/login"
+  end
 end
