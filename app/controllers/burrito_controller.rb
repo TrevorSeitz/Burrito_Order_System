9bd3722df472007ff8d1c1aa4580dbaca1f558de
Trevor Seitz <trevinla@gmail.com>
2018-06-12 11:02:56 -0400

    added burritos/index - connected burrito_controller to list of burritos(burritos/index)

--- app/controllers/burrito_controller.rb
+++ app/controllers/burrito_controller.rb
@@ -8,6 +8,11 @@ class BurritoController < ApplicationController
       # if the user is not logged in - go to login page
       redirect "/login"
     end
-  
+    if !!@user = User.find_by(email: params[:email])
+      redirect "/login"
+    end
+    @burrito = Burrito.new(params)  
+    @user.save
+    redirect "/burritos/new"
   end
 end
