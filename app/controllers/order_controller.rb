107c39039a3c566eacd9a148c401e4d72ab45177
Trevor Seitz <trevinla@gmail.com>
2018-06-12 14:44:06 -0400

    various changes - working on being able to calculate item subtotal

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -3,6 +3,18 @@ class OrderController < ApplicationController
     erb :"/orders/new"
   end
 
+  post "/orders/preview" do
+    binding.pry
+    if !is_logged_in?
+      # if the user is not logged in - go to login page
+      redirect "/login"
+    end
+    if !!@user = User.find_by(email: params[:email])
+      redirect "/login"
+    end
+    binding.pry
+  end
+
   post "/orders/new" do
   
   end
