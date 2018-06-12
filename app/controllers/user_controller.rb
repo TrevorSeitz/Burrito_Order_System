34b9cca56e6244dd08b35d48c6970ce3eeb47769
Trevor Seitz <trevinla@gmail.com>
2018-06-12 12:12:03 -0400

    added buttons to admin page - locked buttons on burritos.index to only admin - completed user_controller connection for admin

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -39,7 +39,6 @@ class UserController < ApplicationController
   end
 
     get "/users/admin" do
-      binding.pry
       @user = User.find_by_id(session[:user_id])
       
       if @user.username == "sam_the_owner"
