0e3cc451988466d559faae8fe2d9c4b10f7ee296
Trevor Seitz <trevinla@gmail.com>
2018-06-13 13:43:59 -0400

    add comments a reorganized the before-do

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,15 @@
 class StoreController < ApplicationController
   before do
+    # Check if User exists
+    if !!@user = User.find_by(email: params[:email])
+      redirect "/users/new"
+    end
+    # Check if User is Logged in
     if !is_logged_in?
       # if the user is not logged in - go to login page
       redirect "/login"
     end
-    if !!@user = User.find_by(email: params[:email])
-      redirect "/login"
-    end
+    # Create @user
     @user = User.find_by_id(session[:user_id])
   end
 
