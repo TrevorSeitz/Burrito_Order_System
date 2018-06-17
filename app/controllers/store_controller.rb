631d5f10c3d89446c52f43b0d4e96a3b7b755184
Trevor Seitz <trevinla@gmail.com>
2018-06-17 13:11:53 -0400

    added comments

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -14,18 +14,24 @@ class StoreController < ApplicationController
   end
 
   get "/stores/new" do
+    # go to new store input page
     erb :"/stores/new"
   end
 
   post "/stores/new" do
+    # check to ensure all ionput fields have content
     if !params.values.all?{|param| !param.empty?}
+      # if not - reload page
       redirect "/stores/new"
     end
     @user = User.find_by_id(session[:user_id])
+    # create new store
     @store = Store.new(params)
     @store.save
+    # add store to user
     @user.store_id = @store.id
     @user.save 
+    # go to user index
     erb :"/users/index"
   end
 
