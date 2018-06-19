7afa24898a9fd48c5d395ab01e4a6fb0ff821b4c
Trevor Seitz <trevinla@gmail.com>
2018-06-19 10:00:40 -0400

    added quantity to burrito for life of order creation

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -20,8 +20,8 @@ class OrderController < ApplicationController
   get "/orders/preview" do
     # collect burritos for order
     @order = Order.find_by_id(@user.order_ids)
-    @order_items = OrderBurrito.all.find_all{|burrito| burrito["order_id"] == @user.order_ids }
-    
+    @order_items = @order.burritos
+
     # preview order
     erb :"/orders/preview"
   end
@@ -29,8 +29,8 @@ class OrderController < ApplicationController
   get "/orders/edit" do
     @order = Order.find_by_id(@user.order_ids)
     @burrito = Burrito.all
-    @order_items = OrderBurrito.all.find_all{|burrito| burrito["order_id"] == @user.order_ids }
-    
+    @order_items = @order.burritos
+
     # edit the order
     erb :"/orders/edit"
   end
@@ -67,13 +67,16 @@ class OrderController < ApplicationController
     @item_count = 0
     # save order to order_burrito table  
     params[:burritos].each do |item|
+      binding.pry
       if item[:quantity].to_i > 0
         # increase item count if quantity is > 0
         @item_count += 1
         # bring in current burrito element
         @burrito = Burrito.find_by_id(item[:id].to_i)
+        @burrito.quantity = item[:quantity].to_i
+        @burrito.save
         # save each ordered burrito to order_burrito
-        OrderBurrito.create(order_id: @order.id, user_id: @user.id, burrito_id: @burrito.id, quantity: item[:quantity].to_i, item_price: @burrito.price)
+        OrderBurrito.create(order_id: @order.id, user_id: @user.id, burrito_id: @burrito.id, quantity: @burrito.quantity, item_price: @burrito.price)
       end
       # if quantity is not > 0 go to next item
     end
@@ -88,9 +91,10 @@ class OrderController < ApplicationController
   patch "/orders/edit" do
     # edit order
     item_count = 0
+    binding.pry
     @order = Order.find_by_id(@user.order_ids)
     # gather all items from order_burrito for this order
-    @order_items = OrderBurrito.all.where(order_id:  @order.id)
+    @order_items = @order.burritos
     #  sort through the edited order 
     params[:burritos].each do |burrito|
       @burrito = Burrito.find_by_id(burrito[:id].to_i)
@@ -99,18 +103,19 @@ class OrderController < ApplicationController
         item_count += 1
       end
       # check to see it item is already in the order
-      @current_item = @order_items.where(burrito_id: burrito[:id].to_i)
-      if @current_item == [] 
-        # if it is not and the quantity > 0 - add to order
-        if burrito[:quantity].to_i >0
-          OrderBurrito.create(order_id: @order.id, user_id: @user.id, burrito_id: @burrito.id, quantity: burrito[:quantity].to_i, item_price: @burrito.price)
-        end
-        # if it is in the order, check to see if the order quantity hads changed
-      elsif @current_item[0].quantity != burrito[:quantity].to_i
-        # if it has, adjust quantiy
-        @current_item[0].quantity = burrito[:quantity].to_i
-        @current_item[0].save
-      end
+      binding.pry
+      #@current_item = @order_items.where(burrito_id: burrito[:id].to_i)
+      # if @order_item == [] 
+      #   # if it is not and the quantity > 0 - add to order
+      #   if burrito[:quantity].to_i >0
+      #     OrderBurrito.create(order_id: @order.id, user_id: @user.id, burrito_id: @burrito.id, quantity: burrito[:quantity].to_i, item_price: @burrito.price)
+      #   end
+      #   # if it is in the order, check to see if the order quantity hads changed
+      # elsif @current_item[0].quantity != burrito[:quantity].to_i
+      #   # if it has, adjust quantiy
+      #   @current_item[0].quantity = burrito[:quantity].to_i
+      #   @current_item[0].save
+      # end
     end
     if item_count < 1
       # if the order now contains 0 items go to error page
@@ -124,6 +129,10 @@ class OrderController < ApplicationController
     # after user confirms order, remove order number from @user
     @user.order_ids = nil
     @user.save
+    Burrito.all.each do |burrito|
+      burrito.quantity = nil
+      burrito.save
+    end
     # go back to user index page
     redirect "/users/index"
   end
